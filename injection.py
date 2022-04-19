76b4b24a02103eca33367b95bd7d1dbe6d47e996
Coutand <bastien.coutand@netcourrier.com>
2022-04-19 20:08:26 +0200

    update css

--- injection.py
+++ injection.py
@@ -20,7 +20,7 @@ know  = ""
 
 while 1:
     for elt in chars:
-        print(elt)
+        print("eeeeeeeeeeeeeeeeeeeeeeeeeeeee : "+elt)
         # new css
         build_css(know+elt)
         sleep(1) # for not to ban
